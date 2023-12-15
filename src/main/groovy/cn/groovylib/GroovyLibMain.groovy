34e545475fb771c9f798cdf46e0af6a2678f253e
Nissining <1319002889@qq.com>
2023-12-15 23:48:44 +0800

    init

---
+++ src/main/groovy/cn/groovylib/GroovyLibMain.groovy
@@ -0,0 +1,12 @@
+package cn.groovylib
+
+import cn.nukkit.plugin.PluginBase
+
+class GroovyLibMain extends PluginBase {
+
+    @Override
+    void onEnable() {
+        getLogger().warning("GroovyLib Enabled!")
+    }
+
+}
